944fab02c70a4e796889343c9edc9c0cfc1efc99
Jasper Rodda <jasper475@gmail.com>
2023-09-07 10:33:42 -0500

    Update app.py commented artifict in CodeBuild and enabled pipeline

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! added artifict in CodeBuild and enabled pipeline'
+    return 'Hello, world! commented artifict in CodeBuild and enabled pipeline'
 
 if __name__ == '__main__':
     app.run()

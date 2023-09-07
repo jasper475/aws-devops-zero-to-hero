19748cff21419a7f7c921f34655face6847e968c
Jasper Rodda <jasper475@gmail.com>
2023-09-06 19:33:12 -0500

    Update app.py artifact2

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Changed Artifact'
+    return 'Hello, world! Changed Artifact 2'
 
 if __name__ == '__main__':
     app.run()

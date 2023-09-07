2d99dc4a717050f57a8cbaa2a4926172cf96f1f7
Jasper Rodda <jasper475@gmail.com>
2023-09-06 19:47:24 -0500

    Update app.py new pipeline

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Artifact removed'
+    return 'Hello, world! Artifact removed new pipeline'
 
 if __name__ == '__main__':
     app.run()

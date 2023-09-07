3a703c3571bbf17858943cba77391d63cd328e90
Jasper Rodda <jasper475@rediffmail.com>
2023-09-07 14:13:52 -0500

    updated stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+echo "Helo, Im stopping App from script stop_container.sh"
 containerId=`sudo docker ps | awk -F " " '{ print $1}'`
 docker rm -f $containerId
